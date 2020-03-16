c265604651524a9c2727e04104e9e670487e8d45
Nick Howland <nhowland@outlook.com>
2020-03-16 13:20:24 -0400

    completed

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.getElementById("myButton").addEventListener("click", function() {
+
     document.getElementById("myButton").innerHTML = "Generating doggo";
     axios.get('https://dog.ceo/api/breed/hound/images/random') 
         .then((response) => {
-            console.log(response.data);
+            console.log(response.data.message);
+            var dogImg = document.createElement('img')
+            dogImg.src = response.data.message;
+            document.getElementById('dogBody').appendChild(dogImg)
+            setTimeout(() => {
+                return document.getElementById("myButton").innerHTML = " Generate doggo"; 
+            }, 200 )
         });
 });
+
